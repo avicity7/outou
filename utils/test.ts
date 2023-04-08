3f6141b0f78ed2688da6b750972451bf71813a0a
avicity7 <74177090+avicity7@users.noreply.github.com>
2023-04-08 11:52:23 +0800

    fix: Remove tests.js

--- utils/test.ts
+++
@@ -1,8 +0,0 @@
-const { DateTime } = require("luxon");
-
-let dt = DateTime.now();
-
-let end = DateTime.fromISO("2023-04-08T09:44:00.537+08:00")
-let start = DateTime.fromISO("2023-04-08T09:43:55.646+08:00")
-
-console.log(end.diff(start,"seconds").toObject().seconds)
